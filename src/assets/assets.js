34e3f51282b4049bc6c6d2ef7582664d12e065b8
Jaymin Rabari <114844115+jayminrabari@users.noreply.github.com>
2024-10-05 17:42:20 +0530

    Update assets.js

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -5,6 +5,7 @@ import p_2 from  './2.webp'
 import p_3 from  './3.webp'
 import p_4 from  './4.webp'
 import p_5 from  './5.webp'
+import p_6 from  './book.png'
 
 export const assets = {
     ironbatman,
@@ -13,5 +14,6 @@ export const assets = {
     p_2,
     p_3,
     p_4,
-    p_5
+    p_5,
+    p_6
 }
